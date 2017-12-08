49941a33d0c6641bfcb5f3e81caaeecd92eb82f2
Nikolay Yermolenko <enik@mail.ua>
2017-12-08 20:26:14 +0200

    inint change

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+//$address = '127.0.0.1';
+//$port = 9000;
+//$sock = socket_create(AF_INET,SOCK_STREAM,0);
+//socket_bind($sock, $address, $port) or die();
+//socket_listen($sock);
+//$client = socket_accept($sock);
+//echo "connection established: $client";
+//socket_close($client);
+//socket_close($sock);
+
+//$var = [55555,77777,88888];
+//print_r($var);
+//$var[10] = 999999;
+//print_r($var);
+//print_r(count($var));
+////print_r($var[3]);
+//
+//$arr = [
+//    'name' => 'sergey',
+//    'age' =>30,
+//    'children'=>['Nazar']
+//
+//];
+//
+//$arr['name'] = 'Vasya';
+//$key = 'name';
+//print_r($arr[$key]);
+//
+//print_r($arr);
+
+for ($i=0; $i<=10; ++$i) {
+    print_r("{$i}\n");
+    
+}
+?>
